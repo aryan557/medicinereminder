77afd4dece62beb80a36cc362f8164e460c86a61
aryan557 <106920680+aryan557@users.noreply.github.com>
2025-01-10 14:47:40 +0530

    Update data.js

--- reminder-frontends/src/data.js
+++ reminder-frontends/src/data.js
@@ -1,4 +1,4 @@
 // export const url = 'https://reminder-backend-tdlh.onrender.com';
 // export const url = 'https://medicine-reminder-application.onrender.com';
-export const url='https://medicinereminder-rvcs.onrender.com';
+export const url='https://getyourmeds.onrender.com';
 
